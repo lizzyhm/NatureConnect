de203c4d51ac1cd0fbca5d256e20819bd634876a
Elizabeth Herrera-Mendez <99587802+lizzyhm@users.noreply.github.com>
2024-11-10 15:50:11 -0500

    Update success_signup.php

--- success_signup.php
+++ success_signup.php
@@ -16,15 +16,11 @@
 	</style>
 </head>
 <body>
-	<header>
-        <h1>NatureConnect</h1>
-        <nav>
-            <a href="index.html">Home</a>
-            <a href="about.html">About Us</a>
-            <a href="resources.html">Resources</a>
-            <a href="login.php">Log In/Sign Up</a>
-        </nav>
-    </header>
-	<p>Congratulations, <b><?php echo $user;?></b>, you have successfully signed up!</p>
+    <div class="header">
+		<?php
+			include "header.php";
+		?>
+    </div>
+    <p>Congratulations, <b><?php echo $user;?></b>, you have successfully signed up!</p>
 </body>
 </html>
