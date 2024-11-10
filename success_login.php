c137e4501eebf05fe77cacb05b24c33a557bc84a
Elizabeth Herrera-Mendez <99587802+lizzyhm@users.noreply.github.com>
2024-11-10 17:47:43 -0500

    Update success_login.php

--- success_login.php
+++ success_login.php
@@ -16,14 +16,15 @@ Nature Connect - Basic Website:
 	<title>Success</title>
 	<style>
 		html {background-color: #8B4513;}
-		body {font-family: 'Times New Roman', serif; display: flex;	flex-direction: column;	justify-content: center; align-items: center; text-align: center; margin: 0; min-height:100vh; }
+		body {font-family: 'Times New Roman', serif; display: flex; flex-direction: column; justify-content: center; align-items: center; text-align: center; margin: 0; min-height:100vh; }
 		.header {width: 100%; color: black; background-color: #8A9A5B; text-align: center; padding: 5px; box-sizing: border-box; margin-top: 0; }
 		header {background-color: #8A9A5B; width: 100%;}
 		nav a {margin: 0 15px; color: #white;text-decoration: none; }
-		.main {background: #D2B48C; width: 100%; box-sizing: border-box; text-align: center; margin-top: 15px; display: flex; flex-direction: column; align-items: center; justify-content: center; flex-grow: 1; }
+		.main {background: #D2B48C; width: 100%; box-sizing: border-box; text-align: center; display: flex; flex-direction: column; align-items: center; justify-content: center; flex-grow: 1; }
 		p {font-size: 20px; }
 		b {color: #e84a5f;}
-		footer {background-color: #8A9A5B; text-align: center; padding: 10px 0; color: white; position: fixed; width: 100%; bottom: 0; }
+		.footer {width: 100%; color: black; background-color: #8A9A5B; text-align: center; padding: 5px; padding-top: 20px; box-sizing: border-box; }
+		footer {background-color: #8A9A5B; text-align: center; padding: 10px 0; color: white; width: 100%; }	
 	</style>
 </head>
 <body>
@@ -35,10 +36,10 @@ Nature Connect - Basic Website:
 	<div class="main">
 		<p>Congratulations, <b><?php echo $user;?></b>, you have successfully logged in!</p>
 	</div>
-    <div class="footer">
-	<?php
-		include "footer.php";
-	?>
-    </div>
+	<div class="footer">
+		<?php
+			include "footer.php";
+		?>
+	</div>
 </body>
 </html>
