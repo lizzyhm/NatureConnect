4047321abdb6382e19d07ef8b93381e1549cdb41
Elizabeth Herrera-Mendez <99587802+lizzyhm@users.noreply.github.com>
2024-11-10 15:59:08 -0500

    Update success_signup.php

--- success_signup.php
+++ success_signup.php
@@ -8,11 +8,16 @@
 	<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Success</title>
-	<link rel="stylesheet" href="styles.css">
 	<style>
-		body {background-color: #fecea8; display: flex; justify-content: center; align-items: center;}
+		html {background-color: #8B4513;}
+		body {font-family: 'Times New Roman', serif; display: flex;	flex-direction: column;	justify-content: center; align-items: center; text-align: center; margin: 0; min-height:100vh; }
+		.header {width: 100%; color: black; background-color: #8A9A5B; text-align: center; padding: 5px; box-sizing: border-box; margin-top: 0; }
+		header {background-color: #8A9A5B; width: 100%;}
+		nav a {margin: 0 15px; color: #white;text-decoration: none; }
+		.main {background: #D2B48C; width: 100%; box-sizing: border-box; text-align: center; margin-top: 15px; display: flex; flex-direction: column; align-items: center; justify-content: center; flex-grow: 1; }
 		p {font-size: 20px; }
-		b {color: #e84a5f}
+		b {color: #e84a5f;}
+		footer {background-color: #8A9A5B; text-align: center; padding: 10px 0; color: white; position: fixed; width: 100%; bottom: 0; }
 	</style>
 </head>
 <body>
@@ -21,8 +26,10 @@
 			include "header.php";
 		?>
     </div>
-    <p>Congratulations, <b><?php echo $user;?></b>, you have successfully signed up!</p>
-     <div class="footer">
+    <div class="main">
+    	<p>Congratulations, <b><?php echo $user;?></b>, you have successfully signed up!</p>
+    </div>
+    <div class="footer">
 	<?php
 		include "footer.php";
 	?>
